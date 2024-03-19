da79427fd0ff3a594b2952582edb401e744ef246
Kauã Paixão <kauantnetsoares@gmail.com>
2024-03-19 10:02:29 -0300

    componente Navbar

---
+++ src/Home.tsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="">
+        <h1 className="bg-blue-400 text-white font text-center mt-4 font-tenor">
+          Hello World
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import logo from '../imgs/logo.png';
+
+export default function Navbar() {
+  return (
+    <div className="flex row justify-between items-center p-4 shadow-lg">
+      <div>
+        <img className="size-16 cursor-pointer" src={logo} alt="" />
+      </div>
+      <div className="flex row justify-center gap-8 ml-8">
+        <button className="font-medium hover:text-blue-400">Postagens</button>
+        <button className="font-medium hover:text-blue-400">Sobre</button>
+      </div>
+      <div className="flex row gap-8">
+        <button className="font-medium hover:text-blue-400 ">Login</button>
+        <button className="font-medium hover:text-blue-400">Cadastre-se</button>
+      </div>
+    </div>
+  );
+}
