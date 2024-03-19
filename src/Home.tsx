85e1490171700189bccf8b570178555a8b4dafe5
Kauã Paixão <kauantnetsoares@gmail.com>
2024-03-19 10:58:40 -0300

    seção superior home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,38 @@
 import Navbar from './components/Navbar';
 
+import social from './imgs/svg/img-social-home-blue.svg';
+
 function Home() {
   return (
     <>
       <Navbar />
-      <div className="">
-        <h1 className="bg-blue-400 text-white font text-center mt-4 font-tenor">
-          Hello World
-        </h1>
-      </div>
+      <section className="h-screen max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-2 justify-center items-center h-screen">
+          <div className="mb-28">
+            <p className="font-roboto font-normal text-6xl pb-16">
+              Compartilhe aqui
+              <br /> o que pensa
+            </p>
+            <div className="flex flex-row gap-8  w-full">
+              <button
+                className="shadow-lg shadow-blue-400 bg-gradient-to-r from-blue-400 to-blue-500 text-white hover:from-blue-500 hover:to-blue-400
+              transition-all rounded-lg w-32 p-2"
+              >
+                Começar
+              </button>
+              <button
+                className="shadow-lg shadow-blue-400 bg-gradient-to-r from-blue-400 to-blue-500 text-white hover:from-blue-500 hover:to-blue-400
+                transition-all rounded-lg w-32 p-2"
+              >
+                Saiba mais
+              </button>
+            </div>
+          </div>
+          <div className="mb-28">
+            <img className="" src={social} alt="" />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import logo from '../imgs/logo.png';
 
 export default function Navbar() {
   return (
-    <div className="flex row justify-between items-center p-4 shadow-lg">
+    <div className="sticky flex row justify-between items-center p-4 shadow-lg">
       <div>
         <img className="size-16 cursor-pointer" src={logo} alt="" />
       </div>
@@ -11,7 +11,7 @@ export default function Navbar() {
         <button className="font-medium hover:text-blue-400">Sobre</button>
       </div>
       <div className="flex row gap-8">
-        <button className="font-medium hover:text-blue-400 ">Login</button>
+        <button className="font-medium hover:text-blue-400">Login</button>
         <button className="font-medium hover:text-blue-400">Cadastre-se</button>
       </div>
     </div>
