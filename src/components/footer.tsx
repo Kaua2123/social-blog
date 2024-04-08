b17b2c9d924bffc45e4abf29f5a7281709cb14e9
Kauã Paixão <kauantnetsoares@gmail.com>
2024-04-08 10:05:12 -0300

    style: Adiciona novos estilos para tornar Home e Footer responsivos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="flex lg:flex-row lg:items-start sm:flex-col sm:items-center  bg-slate-100">
-      <div className="lg:p-20 sm:p-10 flex flex-col gap-2">
+    <div className="flex lg:flex-row lg:items-start flex-col items-center  bg-slate-100">
+      <div className="lg:p-20 p-10 flex flex-col gap-2">
         <p className="text-blue-400 font-poppins">INFORMAÇÃO</p>
         <a href="#" className="text-gray-500">
           Sobre
@@ -14,7 +14,7 @@ export default function Footer() {
         </a>
       </div>
 
-      <div className="lg:p-20 sm:p-10  flex flex-col gap-2">
+      <div className="lg:p-20 p-10  lg:ml-0 ml-8 flex flex-col gap-2">
         <p className="text-blue-400 font-poppins">RECURSOS</p>
         <a href="#" className="text-gray-500">
           Criar um Post
@@ -24,7 +24,7 @@ export default function Footer() {
         </a>
       </div>
 
-      <div className="lg:p-20 sm:p-10  flex flex-col gap-2">
+      <div className="lg:p-20 p-10 lg:ml-0 ml-4 flex flex-col gap-2">
         <p className="text-blue-400 font-poppins">ACESSO RÁPIDO</p>
         <a href="#" className="text-gray-500">
           Home
@@ -37,13 +37,17 @@ export default function Footer() {
         </a>
       </div>
 
-      <div className="lg:p-20 sm:p-10 flex flex-col gap-8">
+      <div className="lg:p-20 p-10 lg:items-start items-center flex flex-col gap-8">
         <p className="text-blue-400 font-poppins">Contate-nos</p>
-        <div className="flex flex-row gap-4 shadow-sm items-center">
-          <input type="text" className="pl-4 p-2" placeholder="Seu email" />
+        <div className="flex flex-col lg:flex-row gap-4 shadow-sm items-center">
+          <input
+            type="text"
+            className="pl-4 p-2 w-56 shadow-sm"
+            placeholder="Seu email"
+          />
           <button
             type="submit"
-            className=" font-poppins hover:opacity-85 w-32 h-10 font-medium text-white  transition-all bg-blue-400 rounded-md"
+            className=" font-poppins hover:opacity-85 lg:w-32 w-56 h-10 font-medium text-white  transition-all bg-blue-400 rounded-md"
           >
             ENVIAR
           </button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,28 +28,32 @@ function Home() {
           <Navbar />
 
           <div className="shadow-sm bg-gradient-to-br from-white via-white to-blue-100">
-            <section className="h-screen max-w-screen-xl mx-auto">
-              <div className="lg:grid grid-cols-2  justify-center items-center h-screen">
+            <section className=" max-w-screen-xl mx-auto">
+              <div className="lg:grid grid-cols-2 justify-center items-center min-h-screen">
                 <div className="mb-28">
-                  <div className="flex flex-col lg:justify-center lg:items-start sm:items-center sm:justify-center sm:mt-20">
-                    <h1 className="font-poppins font-normal lg:text-6xl sm:text-5xl pb-8">
+                  <div className="flex flex-col lg:justify-center lg:items-start items-center justify-center mt-20">
+                    <h1 className="font-poppins font-normal lg:text-6xl text-4xl pb-8">
                       <b className="text-blue-400">Compartilhe</b> aqui
                       <br /> o que pensa
                     </h1>
-                    <p className="font-poppins font-normal lg:text-lg sm:text-base pb-8">
+                    <p className="font-poppins font-normal md:text-lg text-base pb-8">
                       Compartilhe suas ideias, opiniões e experiências com a{' '}
                       <br /> nossa comunidade. Sua voz é importante para nós
                     </p>
                   </div>
 
-                  <div className="flex justify-center lg:justify-start items-center lg:gap-8  gap-10  w-full">
+                  <div
+                    className="flex flex-col items-center lg:flex-row
+                  justify-center lg:justify-start lg:gap-8 gap-4
+                  w-full"
+                  >
                     <Link
                       to={token ? '/posts' : '/login'}
                       className="font-poppins"
                     >
                       <button
                         className="shadow-lg shadow-blue-400 bg-gradient-to-r from-blue-400 to-blue-500 text-white hover:from-blue-500 hover:to-blue-400
-                transition-all rounded-md w-32 p-2"
+                transition-all rounded-md lg:w-32 w-56 p-2"
                       >
                         Começar
                       </button>
@@ -57,14 +61,14 @@ function Home() {
                     <HashLink smooth to={'/#sobre'} className="font-poppins">
                       <button
                         className="shadow-lg shadow-blue-400 bg-gradient-to-r from-blue-400 to-blue-500 text-white hover:from-blue-500 hover:to-blue-400
-                  transition-all rounded-md w-32 p-2"
+                  transition-all rounded-md lg:w-32 w-56 p-2"
                       >
                         Saiba mais
                       </button>
                     </HashLink>
                   </div>
                 </div>
-                <div className="hidden lg:block mb-28">
+                <div className="lg:block mb-28">
                   <img className="" src={social} alt="" />
                 </div>
               </div>
@@ -78,12 +82,12 @@ function Home() {
                 className="flex flex-col text-center justify-center items-center"
               >
                 <div className="w-1/2">
-                  <h2 className="font-poppins lg:text-5xl sm:text-4xl mt-16 mb-8">
+                  <h2 className="font-poppins lg:text-5xl text-2xl mt-16 mb-8">
                     Seja bem-vindo ao nosso{' '}
                     <b className="text-blue-400">blog social</b>, onde cada voz
                     é única e importante.
                   </h2>
-                  <p className="text-lg">
+                  <p className="lg:text-lg font-poppins">
                     Descubra um espaço para compartilhar suas ideias, aprender
                     com os outros e fazer parte de uma comunidade que valoriza a
                     diversidade de pensamentos
@@ -91,17 +95,17 @@ function Home() {
                 </div>
               </div>
 
-              <div className="lg:grid sm:flex sm:flex-col gap-8 sm:items-center md:grid-cols-3 m-10">
+              <div className="lg:grid flex flex-col gap-8 items-center md:grid-cols-3 m-10">
                 <div className="md:border md:shadow-lg border  w-80 rounded-lg min-h-96 p-10">
                   <div className="flex flex-col">
                     <div className="flex justify-center lg:justify-start">
                       <Forward size={70} className="mb-10" />
                     </div>
                     <div className="text-center lg:text-left">
-                      <h3 className="font-poppins mb-3 text-xl">
+                      <h3 className="font-poppins mb-3 md:text-xl">
                         <b className="text-blue-400">Compartilhe</b>{' '}
                       </h3>
-                      <p>
+                      <p className="">
                         Compartilhe dicas úteis e ajude a enriquecer o conteúdo
                         da comunidade.
                       </p>
@@ -115,7 +119,7 @@ function Home() {
                       <MessageCircle size={70} className="mb-10" />
                     </div>
                     <div className="text-center lg:text-left">
-                      <h3 className="font-poppins mb-3 text-xl">
+                      <h3 className="font-poppins mb-3 md:text-xl">
                         <b className="text-blue-400">Interação</b>{' '}
                       </h3>
                       <p>
@@ -132,7 +136,7 @@ function Home() {
                       <Pencil size={70} className="mb-10" />
                     </div>
                     <div className="text-center lg:text-left">
-                      <h3 className="font-poppins mb-3  text-xl">
+                      <h3 className="font-poppins mb-3  md:text-xl">
                         <b className="text-blue-400">Publicação</b>{' '}
                       </h3>
                       <p>
